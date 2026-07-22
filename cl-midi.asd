196b46c69df57ad05f0aaa9ff3a84d3d0a10aace
Bohong Huang <bohonghuang@qq.com>
2026-07-22 14:04:42 +0800

    Revert "Refactor to CLOS message hierarchy rivaling the midi library"
    This reverts commit f60981ff57f208d00637040bf5aaa03dd9c41b23.

--- cl-midi.asd
+++ cl-midi.asd
@@ -1,8 +1,10 @@
 (asdf:defsystem #:cl-midi
+  :depends-on (#:binstruct)
   :components
-  ((:file "src/cl-midi")))
+  ((:file "src/vlq")
+   (:file "src/midi" :depends-on ("src/vlq"))))
 
 (asdf:defsystem #:cl-midi/test
-  :depends-on (#:cl-midi #:parachute #:alexandria)
+  :depends-on (#:cl-midi #:parachute)
   :components
   ((:file "test/cl-midi-test")))
--- test/cl-midi-test.lisp
+++ test/cl-midi-test.lisp
@@ -1,59 +1,64 @@
 (defpackage #:cl-midi.test
-  (:import-from #:midi
-   #:read-midi-file #:midifile
-   #:midifile-format #:midifile-tracks #:midifile-division
-   #:message #:note-off-message #:note-on-message
-   #:control-change-message #:program-change-message
-   #:meta-message #:tempo-message #:end-of-track-message
-   #:text-message #:sequence/track-name-message #:time-signature-message
-   #:key-signature-message #:system-exclusive-message
-   #:message-time #:message-channel #:message-key #:message-velocity
-   #:message-program #:message-tempo #:message-controller #:message-value
-   #:message-text)
-  (:use #:cl #:parachute))
+  (:import-from #:alexandria #:with-gensyms #:once-only #:rcurry)
+  (:import-from #:binstruct
+   #:vlq-base128-be #:read-midi-file
+   #:midi-header-format #:midi-header-division
+   #:midi-file-header #:midi-file-tracks #:midi-track-events
+   #:parser #:parser-run)
+  (:use #:cl #:parachute #:parsonic #:binstruct))
 
 (in-package #:cl-midi.test)
 
 (define-test suite)
 
-(define-test midifile-structure :parent suite
+(defmacro is-rw (test struct &body tests)
+  (with-gensyms (input output)
+    (let ((reader-eval (make-symbol (format nil "~A [~A]" struct '#:reader/eval)))
+          (reader-compiled (make-symbol (format nil "~A [~A]" struct '#:reader/compiled)))
+          (writer-eval (make-symbol (format nil "~A [~A]" struct '#:writer-eval)))
+          (writer-compiled (make-symbol (format nil "~A [~A]" struct '#:writer-compiled))))
+      `(let* ((,reader-eval (lambda (,input) (parser-run (parser ,struct) ,input)))
+              (,reader-compiled (lambda (,input) (parser-run (parser ,struct) (the (simple-array (unsigned-byte 8) (*)) ,input)))))
+         ,@(loop :for (input expected) :in tests
+                 :for bytes := (make-symbol (format nil "~A" input))
+                 :collect (once-only (expected)
+                            `(progn
+                               (let ((,bytes (coerce ,input '(simple-array (unsigned-byte 8) (*)))))
+                                 (let ((binstruct::*positions* nil))
+                                   (is ,test ,expected (funcall ,reader-eval ,bytes)))
+                                 (let ((binstruct::*positions* nil))
+                                   (is ,test ,expected (funcall ,reader-compiled ,bytes)))
+                                 (let ((,output (make-array 0 :element-type '(unsigned-byte 8) :adjustable t :fill-pointer 0)))
+                                   (let ((binstruct::*positions* nil))
+                                     ,(binstruct::expand-writer-type-unit struct :output `(binstruct::vector-emitter-output ,output) :value expected)
+                                     (binstruct::flush-pointer-positions))
+                                   (let* ((,writer-eval (coerce ,output '(simple-array (unsigned-byte 8) (*))))
+                                          (,writer-compiled ,writer-eval))
+                                     (let ((binstruct::*positions* nil))
+                                       (is ,test ,expected (funcall ,reader-eval ,writer-eval)))
+                                     (let ((binstruct::*positions* nil))
+                                       (is ,test ,expected (funcall ,reader-compiled ,writer-compiled)))))))))))))
+
+(defmacro is-rw-equalp (struct &body tests)
+  `(is-rw equalp ,struct . ,tests))
+
+(define-test vlq :parent suite
+  (let ((read-vlq (lambda (bytes)
+                    (let ((binstruct::*positions* nil))
+                      (parser-run (parser (vlq-base128-be))
+                                  (coerce bytes '(simple-array (unsigned-byte 8) (*))))))))
+    (is = 0 (funcall read-vlq #(#x00)))
+    (is = 127 (funcall read-vlq #(#x7F)))
+    (is = 128 (funcall read-vlq #(#x81 #x00)))
+    (is = 840 (funcall read-vlq #(#x86 #x48)))))
+
+(define-test midi-file-read :parent suite
   (loop :for file :in (directory #P"~/.quicklisp/local-projects/cl-midi/test/mid/*.mid")
-        :do (let ((midi (read-midi-file file)))
-              (is = 1 (midifile-format midi))
-              (is = 48 (midifile-division midi))
-              (true (plusp (length (midifile-tracks midi))))
-              (true (plusp (length (first (midifile-tracks midi))))))))
-
-(define-test message-types :parent suite
-  (let ((midi (read-midi-file #P"~/.quicklisp/local-projects/cl-midi/test/mid/SEQ_BA_CHANP.mid")))
-    (let ((track0 (first (midifile-tracks midi))))
-      ;; Track 0 should have meta events (tempo, text) and channel events
-      (true (some #'(lambda (m) (typep m 'tempo-message)) track0))
-      (true (some #'(lambda (m) (typep m 'text-message)) track0))
-      ;; Some track should have note-on messages
-      (true (some #'(lambda (m) (typep m 'note-on-message))
-                  (alexandria:flatten (midifile-tracks midi)))))))
-
-(define-test absolute-time :parent suite
-  (loop :for file :in (directory #P"~/.quicklisp/local-projects/cl-midi/test/mid/*.mid")
-        :do (let ((midi (read-midi-file file)))
-              (loop :for track :in (midifile-tracks midi)
-                    :do (loop :for msg :in track
-                             :for prev := 0 :then (message-time msg)
-                             :do (true (>= (message-time msg) prev)))))))
-
-(define-test note-on-values :parent suite
-  (let ((midi (read-midi-file #P"~/.quicklisp/local-projects/cl-midi/test/mid/SEQ_BA_CHANP.mid")))
-    (loop :for track :in (midifile-tracks midi)
-          :do (loop :for msg :in track
-                    :when (typep msg 'note-on-message)
-                      :do (true (<= 0 (message-key msg) 127))
-                          (true (<= 0 (message-velocity msg) 127))
-                          (true (<= 0 (message-channel msg) 15)))))
-
-  (let ((midi (read-midi-file #P"~/.quicklisp/local-projects/cl-midi/test/mid/SEQ_BA_CHANP.mid")))
-    (loop :for track :in (midifile-tracks midi)
-          :do (loop :for msg :in track
-                    :when (typep msg 'control-change-message)
-                      :do (true (<= 0 (message-controller msg) 127))
-                          (true (<= 0 (message-value msg) 127))))))
+        :do (let* ((arr (with-open-file (s file :direction :input :element-type '(unsigned-byte 8))
+                          (let ((a (make-array (file-length s) :element-type '(unsigned-byte 8))))
+                            (read-sequence a s) a)))
+                   (midi (read-midi-file arr)))
+              (is = 1 (midi-header-format (midi-file-header midi)))
+              (is = 48 (midi-header-division (midi-file-header midi)))
+              (true (plusp (length (midi-file-tracks midi))))
+              (true (plusp (length (midi-track-events (aref (midi-file-tracks midi) 0))))))))
