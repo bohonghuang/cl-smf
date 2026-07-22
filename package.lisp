1a0be0bcf56af0daf31c7860ae7b60268dba0723
Bohong Huang <bohonghuang@qq.com>
2026-07-23 00:53:25 +0800

    MIDI -> SMF

--- package.lisp
+++ package.lisp
@@ -4,22 +4,12 @@
 
 (in-package #:smf)
 
-(defbinstruct midi-event ())
+(defbinstruct smf-event ())
 
-(defbinstruct (midi-channel-event (:include midi-event)) ())
-
-(defmacro define-midi-channel-event ((name status) &body fields)
-  `(defbinstruct (,name (:include midi-channel-event) (:endian :big)) ()
-     (channel ,status :type (map (or . ,(loop :for i :from #x00 :to #x0F
-                                              :collect `(satisfies (unsigned-byte 8) (curry #'eql ,(+ status i)))))
-                                 (curry #'ldb (byte 4 0))
-                                 (rcurry #'dpb (byte 4 0) ,status)))
-     . ,fields))
-
-(defmacro define-midi-event ((name status) &body fields)
+(defmacro define-smf-event ((name status) &body fields)
   (let* ((name-list (ensure-list name))
          (has-include (some (lambda (opt) (and (consp opt) (find :include (rest opt)))) name-list)))
-    `(defbinstruct (,@name-list ,@(unless has-include '((:include midi-event))) (:endian :big)) ()
+    `(defbinstruct (,@name-list ,@(unless has-include '((:include smf-event))) (:endian :big)) ()
        (nil ,status :type (satisfies (unsigned-byte 8) (curry #'eql ,status)))
        . ,fields)))
 
@@ -27,29 +17,39 @@
 ;; Channel events ;;
 ;;;;;;;;;;;;;;;;;;;;
 
-(define-midi-channel-event (midi-note-off-event #x80)
+(defbinstruct (smf-channel-event (:include smf-event)) ())
+
+(defmacro define-smf-channel-event ((name status) &body fields)
+  `(defbinstruct (,name (:include smf-channel-event) (:endian :big)) ()
+     (channel ,status :type (map (or . ,(loop :for i :from #x00 :to #x0F
+                                              :collect `(satisfies (unsigned-byte 8) (curry #'eql ,(+ status i)))))
+                                 (curry #'ldb (byte 4 0))
+                                 (rcurry #'dpb (byte 4 0) ,status)))
+     . ,fields))
+
+(define-smf-channel-event (smf-note-off-event #x80)
   (note 0 :type (unsigned-byte 8))
   (velocity 0 :type (unsigned-byte 8)))
 
-(define-midi-channel-event (midi-note-on-event #x90)
+(define-smf-channel-event (smf-note-on-event #x90)
   (note 0 :type (unsigned-byte 8))
   (velocity 0 :type (unsigned-byte 8)))
 
-(define-midi-channel-event (midi-poly-pressure-event #xA0)
+(define-smf-channel-event (smf-poly-pressure-event #xA0)
   (note 0 :type (unsigned-byte 8))
   (pressure 0 :type (unsigned-byte 8)))
 
-(define-midi-channel-event (midi-controller-event #xB0)
+(define-smf-channel-event (smf-controller-event #xB0)
   (controller 0 :type (unsigned-byte 8))
   (value 0 :type (unsigned-byte 8)))
 
-(define-midi-channel-event (midi-program-change-event #xC0)
+(define-smf-channel-event (smf-program-change-event #xC0)
   (program 0 :type (unsigned-byte 8)))
 
-(define-midi-channel-event (midi-channel-pressure-event #xD0)
+(define-smf-channel-event (smf-channel-pressure-event #xD0)
   (pressure 0 :type (unsigned-byte 8)))
 
-(define-midi-channel-event (midi-pitch-bend-event #xE0)
+(define-smf-channel-event (smf-pitch-bend-event #xE0)
   (lsb 0 :type (unsigned-byte 8))
   (msb 0 :type (unsigned-byte 8)))
 
@@ -57,55 +57,55 @@
 ;; Mode messages ;;
 ;;;;;;;;;;;;;;;;;;;
 
-(define-midi-channel-event (midi-mode-message #xB0))
+(define-smf-channel-event (smf-mode-message #xB0))
 
-(defmacro define-midi-mode-message (name status)
-  `(define-midi-event ((,name (:include midi-mode-message)) ,status)
+(defmacro define-smf-mode-message (name status)
+  `(define-smf-event ((,name (:include smf-mode-message)) ,status)
      (value 0 :type (unsigned-byte 8))))
 
-(define-midi-mode-message midi-reset-all-controllers-event #x79)
+(define-smf-mode-message smf-reset-all-controllers-event #x79)
 
-(define-midi-mode-message midi-local-control-event #x7A)
+(define-smf-mode-message smf-local-control-event #x7A)
 
-(define-midi-mode-message midi-all-notes-off-event #x7B)
+(define-smf-mode-message smf-all-notes-off-event #x7B)
 
-(define-midi-mode-message midi-omni-mode-off-event #x7C)
+(define-smf-mode-message smf-omni-mode-off-event #x7C)
 
-(define-midi-mode-message midi-omni-mode-on-event #x7D)
+(define-smf-mode-message smf-omni-mode-on-event #x7D)
 
-(define-midi-mode-message midi-mono-mode-on-event #x7E)
+(define-smf-mode-message smf-mono-mode-on-event #x7E)
 
-(define-midi-mode-message midi-poly-mode-on-event #x7F)
+(define-smf-mode-message smf-poly-mode-on-event #x7F)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; System common messages ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-midi-event (midi-timing-code-event #xF1)
+(define-smf-event (smf-timing-code-event #xF1)
   (code 0 :type (unsigned-byte 8)))
 
-(define-midi-event (midi-song-position-pointer-event #xF2)
+(define-smf-event (smf-song-position-pointer-event #xF2)
   (lsb 0 :type (unsigned-byte 8))
   (msb 0 :type (unsigned-byte 8)))
 
-(define-midi-event (midi-song-select-event #xF3)
+(define-smf-event (smf-song-select-event #xF3)
   (song 0 :type (unsigned-byte 8)))
 
-(define-midi-event (midi-tune-request-event #xF6))
+(define-smf-event (smf-tune-request-event #xF6))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; System real-time messages ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-midi-event (midi-timing-clock-event #xF8))
+(define-smf-event (smf-timing-clock-event #xF8))
 
-(define-midi-event (midi-start-sequence-event #xFA))
+(define-smf-event (smf-start-sequence-event #xFA))
 
-(define-midi-event (midi-continue-sequence-event #xFB))
+(define-smf-event (smf-continue-sequence-event #xFB))
 
-(define-midi-event (midi-stop-sequence-event #xFC))
+(define-smf-event (smf-stop-sequence-event #xFC))
 
-(define-midi-event (midi-active-sensing-event #xFE))
+(define-smf-event (smf-active-sensing-event #xFE))
 
 ;;;;;;;;;
 ;; VLQ ;;
@@ -137,11 +137,11 @@
 ;; SysEx events ;;
 ;;;;;;;;;;;;;;;;;;
 
-(define-midi-event (midi-sysex-event #xF0)
+(define-smf-event (smf-sysex-event #xF0)
   (len 0 :type vlq)
   (data (make-array 0 :element-type '(unsigned-byte 8)) :type (simple-array (unsigned-byte 8) (len))))
 
-(define-midi-event (midi-authorization-sysex-event #xF7)
+(define-smf-event (smf-authorization-sysex-event #xF7)
   (len 0 :type vlq)
   (data (make-array 0 :element-type '(unsigned-byte 8)) :type (simple-array (unsigned-byte 8) (len))))
 
@@ -149,81 +149,81 @@
 ;; Meta events ;;
 ;;;;;;;;;;;;;;;;;
 
-(define-midi-event ((midi-meta-event (:include midi-event)) #xFF))
+(define-smf-event ((smf-meta-event (:include smf-event)) #xFF))
 
-(defmacro define-midi-meta-event ((name status) &body fields)
-  `(define-midi-event ((,name (:include midi-meta-event)) ,status)
+(defmacro define-smf-meta-event ((name status) &body fields)
+  `(define-smf-event ((,name (:include smf-meta-event)) ,status)
      (len 0 :type vlq)
      ,@fields))
 
-(define-midi-meta-event (midi-sequence-number-event #x00)
+(define-smf-meta-event (smf-sequence-number-event #x00)
   (ssss 0 :type (unsigned-byte 16)))
 
-(define-midi-meta-event (midi-text-event #x01)
+(define-smf-meta-event (smf-text-event #x01)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-copyright-event #x02)
+(define-smf-meta-event (smf-copyright-event #x02)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-sequence-track-name-event #x03)
+(define-smf-meta-event (smf-sequence-track-name-event #x03)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-instrument-name-event #x04)
+(define-smf-meta-event (smf-instrument-name-event #x04)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-lyric-event #x05)
+(define-smf-meta-event (smf-lyric-event #x05)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-marker-event #x06)
+(define-smf-meta-event (smf-marker-event #x06)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-cue-point-event #x07)
+(define-smf-meta-event (smf-cue-point-event #x07)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-program-name-event #x08)
+(define-smf-meta-event (smf-program-name-event #x08)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-device-name-event #x09)
+(define-smf-meta-event (smf-device-name-event #x09)
   (text "" :type (simple-base-string len)))
 
-(define-midi-meta-event (midi-channel-prefix-event #x20)
+(define-smf-meta-event (smf-channel-prefix-event #x20)
   (cc 0 :type (unsigned-byte 8)))
 
-(define-midi-meta-event (midi-end-of-track-event #x2F))
+(define-smf-meta-event (smf-end-of-track-event #x2F))
 
-(define-midi-meta-event (midi-tempo-event #x51)
+(define-smf-meta-event (smf-tempo-event #x51)
   (tttttt 0 :type (unsigned-byte 24)))
 
-(define-midi-meta-event (midi-smpte-offset-event #x54)
+(define-smf-meta-event (smf-smpte-offset-event #x54)
   (hr 0 :type (unsigned-byte 8))
   (mn 0 :type (unsigned-byte 8))
   (se 0 :type (unsigned-byte 8))
   (fr 0 :type (unsigned-byte 8))
   (ff 0 :type (unsigned-byte 8)))
 
-(define-midi-meta-event (midi-time-signature-event #x58)
+(define-smf-meta-event (smf-time-signature-event #x58)
   (nn 0 :type (unsigned-byte 8))
   (dd 0 :type (unsigned-byte 8))
   (cc 0 :type (unsigned-byte 8))
   (bb 0 :type (unsigned-byte 8)))
 
-(define-midi-meta-event (midi-key-signature-event #x59)
+(define-smf-meta-event (smf-key-signature-event #x59)
   (sf 0 :type (signed-byte 8))
   (mi 0 :type (unsigned-byte 8)))
 
-(define-midi-meta-event (midi-sequencer-specific-event #x7F)
+(define-smf-meta-event (smf-sequencer-specific-event #x7F)
   (data (make-array 0 :element-type '(unsigned-byte 8)) :type (simple-array (unsigned-byte 8) (len))))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun midi-event-classes (&optional (class (find-class 'midi-event)))
+  (defun smf-event-classes (&optional (class (find-class 'smf-event)))
     (or (loop :for class :in (c2mop:class-direct-subclasses class)
-              :nconc (midi-event-classes class))
+              :nconc (smf-event-classes class))
         (list class))))
 
 (defbinstruct (smf-track-event (:endian :big)) (end)
   (nil 0 :type (satisfies position (rcurry #'< end)))
   (delta 0 :type vlq)
-  (event nil :type (or . #.(mapcar #'class-name (midi-event-classes)))))
+  (event nil :type (or . #.(mapcar #'class-name (smf-event-classes)))))
 
 (defbinstruct (smf-track (:endian :big)) ()
   (nil #.(coerce "MTrk" 'simple-base-string) :type (satisfies (simple-base-string 4)))
